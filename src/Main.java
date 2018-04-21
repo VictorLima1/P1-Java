33b84ad2f4a0811bcd1424e8ff92e38c60e93ff9
VictorLima1 <victormanoel119@gmail.com>
2018-04-21 19:52:12 -0300

    Gerando novos veículos e testando as cores no terminal do Linux

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		int i = 0;
 		while (i == 0) {
 			m.atualizaMundo();
-			Thread.sleep(10);  
+			Thread.sleep(500);  
 		}
 		
 	
--- src/Mundo.java
+++ src/Mundo.java
@@ -39,19 +39,19 @@ public class Mundo {
 	
 	public void geraVeiculos() {
 		for(int i = 0; i < 10; i++) {
-			ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde"));
+			ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
 			while (mapa[ca.get(i).getX()][ca.get(i).getY()] == 2) {
 				ca.get(i).setX();
 				ca.get(i).setY();
 			}
 
-			da.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "verde"));
+			da.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "amarelo", false));
 			while (mapa[da.get(i).getX()][da.get(i).getY()] == 2) {
 				da.get(i).setX();
 				da.get(i).setY();
 			}
 
-			ma.add(new Moto((v.setX()+1), (v.setY()+1), 3, "azul"));
+			ma.add(new Moto((v.setX()+1), (v.setY()+1), 3, "vermelho", false));
 			while (mapa[ma.get(i).getX()][ma.get(i).getY()] == 2) {
 				ma.get(i).setX();
 				ma.get(i).setY();
@@ -137,29 +137,29 @@ public class Mundo {
 				for(int j = 0; j < 60; j++) {
 					
 					if(mapa[i][j] == 1) {
-						System.out.print("1");
+						System.out.print("\u001b[47m \033[0m");
 					}
 					else if(mapa[i][j] == 0) {
 						System.out.print(" ");
 					}
 					
 					else if(mapa[i][j] == 2) {
-						System.out.print("X");
-						//System.out.print("\u001b[42m \033[0m");
+						//System.out.print("X");
+						System.out.print("\u001b[42m \033[0m");
 					}
 					else if(mapa[i][j] == 3){
-						System.out.print("C");
-						//System.out.print("\u001b[44m \033[0m");
+						//System.out.print("C");
+						System.out.print("\u001b[44m \033[0m");
 						carros++;
 					}
 					else if(mapa[i][j] == 4){
-						System.out.print("D");
-						//System.out.print("\u001b[43;1m \033[0m");
+						//System.out.print("D");
+						System.out.print("\u001b[43;1m \033[0m");
 						caminhoes++;
 					}
 					else if(mapa[i][j] == 5){
-						System.out.print("M");
-						//System.out.print("\u001b[41m \033[0m");
+						//System.out.print("M");
+						System.out.print("\u001b[41m \033[0m");
 						motos++;
 					}
 					
@@ -168,6 +168,8 @@ public class Mundo {
 				System.out.println("");
 			}	
 			
+			System.out.print("\u001b[44m \033[0m");
+			System.out.println(" Carro");
 			System.out.println("Número de carros: " + carros);
 			System.out.println("Número de caminhões: " + caminhoes);
 			System.out.println("Número de motos: " + motos);
@@ -186,6 +188,7 @@ public class Mundo {
 			ma.get(i).move(ma.get(i));
 		}
 		
+		geraVeiculo();
 		detectaColisao();
 		refazMapa();
 		desenhaMundo();
@@ -264,11 +267,56 @@ public class Mundo {
 			}
 		}
 	}
-}
-	/*
-	public void geraDois() {
-		for(int i = 0; i < 2; i++) {
-			ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde"));
+	
+	public void geraVeiculo() {
+		for(int a = 0; a < ca.size(); a++) {
+			int x = ca.get(a).getX();
+			int y = ca.get(a).getY();
+				for(int i = 0; i < 30; i ++) {
+					for(int j = 0; j < 10; j++) {
+						if(i == x && j == y) {
+							if(mapa[i][j] == 2 && ca.get(a).getFabrica() == false) {
+								ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
+								}
+							}
+						}
+					}
+				}
+		
+		for(int a = 0; a < da.size(); a++) {
+			int x = da.get(a).getX();
+			int y = da.get(a).getY();
+				for(int i = 0; i < 30; i ++) {
+					for(int j = 0; j < 10; j++) {
+						if(i == x && j == y) {
+							if(mapa[i][j] == 2 && da.get(a).getFabrica() == false) {
+								da.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "verde", false));
+								}
+							}
+						}
+					}
+				}
+		
+	
+		for(int a = 0; a < ma.size(); a++) {
+			int x = ma.get(a).getX();
+			int y = ma.get(a).getY();
+				for(int i = 0; i < 30; i ++) {
+					for(int j = 0; j < 10; j++) {
+						if(i == x && j == y) {
+							if(mapa[i][j] == 2 && ma.get(a).getFabrica() == false) {
+								ma.add(new Moto((v.setX()+1), (v.setY()+1), 3, "vermelho", false));
+							}
+						}
+					}
+				}
 		}
-	}*/
+	}
+}
+	
+	
+	
+
+	
+	
 	
